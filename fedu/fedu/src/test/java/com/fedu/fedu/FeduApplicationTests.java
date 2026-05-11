a3b5668a2304211a7ee2fe204545ba746cb41619
Do Trung Hieu <hieudt271004@gmail.com>
2026-05-11 11:58:59 +0700

    chore: init project
    initialize project

---
+++ fedu/fedu/src/test/java/com/fedu/fedu/FeduApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fedu.fedu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeduApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
