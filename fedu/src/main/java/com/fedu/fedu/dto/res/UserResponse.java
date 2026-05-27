f45e09c31b393f4a503bbf243051edb9a4bffc26
minluvpkl <dangbaominh1010.hn@gmail.com>
2026-05-27 17:56:24 +0700

    feat: edit profile feature
    thêm chức năng chỉnh sửa profile

--- fedu/src/main/java/com/fedu/fedu/dto/res/UserResponse.java
+++ fedu/src/main/java/com/fedu/fedu/dto/res/UserResponse.java
@@ -1,25 +1,33 @@
 package com.fedu.fedu.dto.res;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fedu.fedu.utils.enums.Gender;
 import com.fedu.fedu.utils.enums.UserStatus;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Getter;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.List;
 
-@Getter
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
-public class UserResponse implements Serializable {
-    private Long userId;
+public class UserResponse {
+    
+    private long userId;
     private String email;
     private String firstName;
     private String lastName;
-    private String avatarUrl;
-    private List<String> roles;
-    private UserStatus status;
-    private Gender gender;
-    private LocalDate bod;
     private String phone;
+    private Gender gender;
+    
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private LocalDate bod;
+    
+    private String avatarUrl;
+    private UserStatus status;
+    private List<String> roles;
 }
